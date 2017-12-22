828093c655330121037e07535dbb36395e27d3d6
Bingo Du <tarvos21@gmail.com>
2017-12-23 00:24:57 +0800

    Refactor

--- static/javascripts/post-home.js
+++ static/javascripts/post-home.js
@@ -3,23 +3,20 @@ $(document).ready(function() {
   // change style and add arrow icon when hovered 
   $('.centered.column > .ui.raised.segment')
     .hover(
-        function() {
-          $(this).addClass('bigger');
-          $('.sub.header').addClass('ui grey');
-          $("<div class='ui right floated basic segment'><i class='huge inverted yellow angle right icon'></i></div>").insertAfter($(this).find('h1.ui.header'));
-        },
-        function() {
-          $(this).removeClass('black inverted bigger');
-          $('.sub.header').removeClass('ui grey');
-          $(".ui.right.floated.segment").remove();
-        });
+      function() {
+        $(this).addClass('bigger');
+      },
+      function() {
+        $(this).removeClass('bigger');
+      }
+    );
 
   // load the blog page when the entire segment is clicked 
   $('.centered.column > .ui.raised.segment')
     .click(
-        function() {
-          window.location = $(this).find("h1.ui.header a").attr("href");
-          return false;
-        });
+      function() {
+        window.location = $(this).find("h1.ui.header a").attr("href");
+        return false;
+      });
 
 });
